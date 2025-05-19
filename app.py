5ca645192f9a8889a3e232df7119ca747f5c8842
Abdelilah Elgallati <abdelilah.elgallati6@gmail.com>
2025-05-19 22:44:38 +0100

    Add the configuration of vercel

--- app.py
+++ app.py
@@ -252,4 +252,4 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     
     # Run the app
-    app.run(host='0.0.0.0', port=port) 
+    app.run(host='0.0.0.0', port=port, debug=True) 
