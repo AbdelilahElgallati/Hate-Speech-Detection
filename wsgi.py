88307806593c4a7e61a9fd9fe4df4cc8ff6069b4
Abdelilah Elgallati <abdelilah.elgallati6@gmail.com>
2025-05-24 18:01:05 +0100

    Add flask page to test the model

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,23 @@
 import sys
 import os
 
+# Add virtual environment path
+venv_path = '/home/AbdelilahElgallati/.virtualenvs/hate-speech-env/lib/python3.9/site-packages'
+if venv_path not in sys.path:
+    sys.path.insert(0, venv_path)
+
 # Add your project directory to the Python path
-path = '/home/yourusername/Hate-Speech-Detection'  # Replace with your PythonAnywhere username
+path = '/home/AbdelilahElgallati/Hate-Speech-Detection'  # Replace with your PythonAnywhere username
 if path not in sys.path:
-    sys.path.append(path)
+    sys.path.insert(0, path)
 
 # Set environment variables
 os.environ['PYTHONPATH'] = path
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Suppress TensorFlow warnings
 
 # Import your Flask app
-from app import app as application 
+from app import app as application
+
+# Enable error logging
+import logging
+logging.basicConfig(stream=sys.stderr) 
